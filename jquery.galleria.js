e125e15d89113d401275cda8d07cc837307b8aca
Shiro-Kun <roykhan85@gmail.com>
2016-03-21 20:26:07 +0700

    Added files via upload

---
+++ jquery.galleria.js
@@ -0,0 +1,72 @@
+/**
+ * Galleria (http://monc.se/kitchen)
+ *
+ * Galleria is a javascript image gallery written in jQuery. 
+ * It loads the images one by one from an unordered list and displays thumbnails when each image is loaded. 
+ * It will create thumbnails for you if you choose so, scaled or unscaled, 
+ * centered and cropped inside a fixed thumbnail box defined by CSS.
+ * 
+ * The core of Galleria lies in it's smart preloading behaviour, snappiness and the fresh absence 
+ * of obtrusive design elements. Use it as a foundation for your custom styled image gallery.
+ *
+ * MAJOR CHANGES v.FROM 0.9
+ * Galleria now features a useful history extension, enabling back button and bookmarking for each image.
+ * The main image is no longer stored inside each list item, instead it is placed inside a container
+ * onImage and onThumb functions lets you customize the behaviours of the images on the site
+ *
+ * Tested in Safari 3, Firefox 2, MSIE 6, MSIE 7, Opera 9
+ * 
+ * Version 1.0
+ * Februari 21, 2008
+ *
+ * Copyright (c) 2008 David Hellsing (http://monc.se)
+ * Licensed under the GPL licenses.
+ * http://www.gnu.org/licenses/gpl.txt
+ **/
+
+(function($){
+
+var $$;
+
+
+/**
+ * 
+ * @desc Convert images from a simple html <ul> into a thumbnail gallery
+ * @author David Hellsing
+ * @version 1.0
+ *
+ * @name Galleria
+ * @type jQuery
+ *
+ * @cat plugins/Media
+ * 
+ * @example $('ul.gallery').galleria({options});
+ * @desc Create a a gallery from an unordered list of images with thumbnails
+ * @options
+ *   insert:   (selector string) by default, Galleria will create a container div before your ul that holds the image.
+ *             You can, however, specify a selector where the image will be placed instead (f.ex '#main_img')
+ *   history:  Boolean for setting the history object in action with enabled back button, bookmarking etc.
+ *   onImage:  (function) a function that gets fired when the image is displayed and brings the jQuery image object.
+ *             You can use it to add click functionality and effects.
+ *             f.ex onImage(image) { image.css('display','none').fadeIn(); } will fadeIn each image that is displayed
+ *   onThumb:  (function) a function that gets fired when the thumbnail is displayed and brings the jQuery thumb object.
+ *             Works the same as onImage except it targets the thumbnail after it's loaded.
+ *
+**/
+
+$$ = $.fn.galleria = function($options) {
+	
+	// check for basic CSS support
+	if (!$$.hasCSS()) { return false; }
+	
+	// init the modified history object
+	$.historyInit($$.onPageLoad);
+	
+	// set default options
+	var $defaults = {
+		history     : true,
+		clickNext   : true,
+		
+	};
+}
+});
